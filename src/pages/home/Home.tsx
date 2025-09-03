6fb06788e56b7a96689ce9de347e55b4152ef330
Bruna Sadi Duarte <101605299+brunasadi@users.noreply.github.com>
2025-09-03 09:30:44 -0300

    aula0309

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,39 +4,23 @@ function Home() {
   return (
     // fragmento pra agrupar lista de elementos dentro de uma unica situação agrupa tds elementos html <> </>
     <>
-      <div
-        style={{
-          width: "100vw",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div>
-          <div
-            style={{
-              width: "80vw",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h2>Seja bem vindo</h2>
-            <p>Expresse seus pensamentos e opiniões</p>
-
-            <div
-              style={{
-                width: "80vw",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img
-                src="https://i.imgur.com/VpwApCU.png"
-                alt="img da pag home"
-                width="400px"
-              />
+      <div className="bg-indigo-900 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Sejam Bem- Vindos</h2>
+            <p className="text-xl">Expresse aqui seus pensamentos</p>
+            <div className="flex justify-around gap-4">
+              <div className="rounded text-white border-white border-solid border-2 py-2 px-4"></div>
+              Nova Postagem
             </div>
+          </div>
+
+          <div className="flex justify-center">
+            <img
+              src="https://i.imgur.com/fyfri1v.png"
+              alt=""
+              className="w-2/3"
+            />
           </div>
         </div>
       </div>
